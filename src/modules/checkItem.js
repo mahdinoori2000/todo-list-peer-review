5a2c56b174a7865df44bef3bdd70a73cc7bacd10
Mahdi Noori <mahdinoori2389@gmail.com>
2023-06-19 23:07:37 +0430

    Update checkItem.js

--- src/modules/checkItem.js
+++ src/modules/checkItem.js
@@ -1,13 +1,3 @@
-// MARK LIST ITEM
-// const checkElement = (checkbox, item) => {
-//   if (checkbox.checked) {
-//     checkbox.nextElementSibling.style.textDecoration = 'line-through';
-//     item.completed = true;
-//   } else {
-//     checkbox.nextElementSibling.style.textDecoration = 'none';
-//     item.completed = false;
-//   }
-// };
 
 const checkElement = () => {
 
